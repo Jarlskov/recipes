949c5be314d25a0ab612ca5be96fcd40e15895e6
Jesper Jarlskov <jjarlskov@toogoodtogo.com>
2025-09-26 15:58:48 +0200

    Remove the default user homepage and just use /Dish

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -13,6 +13,12 @@ class HomeController extends AbstractController
     #[Route('/', name: 'home')]
     public function index(): Response
     {
+        // Redirect logged-in users directly to dishes
+        if ($this->getUser()) {
+            return $this->redirectToRoute('dish_index');
+        }
+        
+        // Show welcome page for non-logged-in users
         return $this->render('home/index.html.twig');
     }
 }
